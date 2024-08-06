f5f70fb61f7f56601e3e9330c6e53b3daaf5611e
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 19:21:56 +0530

    Add files via upload

---
+++ logical_operators.java
@@ -0,0 +1,39 @@
+
+
+public class logical_operators {
+    public static void main(String[] args) {
+        // && OPERATOR
+        boolean a=true;
+        boolean b=false;
+        int z=30;
+        int v=20;
+        System.out.println("the condition is:"+( z > v && a));
+// OR OPERATOR:
+
+boolean aa= true;
+boolean yy=false;
+int kk = 70;
+int ll = 40;
+System.out.println("The condition is :"+(ll <  kk || aa));
+
+//NOT  OPERATOR:
+
+
+boolean cat = true;
+boolean dog = false;
+System.out.println("the condition is:"+(!cat));
+    
+}
+
+
+    }
+
+
+
+ 
+
+        
+
+        
+    
+    
